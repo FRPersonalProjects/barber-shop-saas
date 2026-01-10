95aac7819087bdf6fb3961f4015aaad96b82fa9d
FelipeRibeiro12 <felipe.gabriel@ges.inatel.br>
2026-01-10 10:39:45 -0300

    feat: add @nestjs/swagger for API documentation and remove unused spec files

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder, OpenAPIObject } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 
-console.log('Database URL:', process.env.DATABASE_URL);
+// console.log('Database URL:', process.env.DATABASE_URL);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .addServer('http://localhost:8080')
+    .setTitle('TrimTech')
+    .setDescription('Saas API')
+    .setVersion('1.0')
+    .addTag('user')
+    .build();
+
+  const documentFactory = (): OpenAPIObject =>
+    SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
